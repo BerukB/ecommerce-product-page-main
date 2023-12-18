747f6ae62ae624754469c9de7257f0456cc68734
BerukB <berukberhanua@gmail.com>
2023-12-18 19:09:43 +0300

    initial commit

---
+++ ecommerce-product-page-main/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="container mx-auto full bg-pink-400">
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  </div>
+</template>
+
