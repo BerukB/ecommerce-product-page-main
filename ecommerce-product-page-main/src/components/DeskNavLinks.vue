ee711c8afa426d546f6a14779ca3e79e6767d0c5
BerukB <berukberhanua@gmail.com>
2023-12-20 00:14:41 +0300

    NavBar half completed

---
+++ ecommerce-product-page-main/src/components/DeskNavLinks.vue
@@ -0,0 +1,23 @@
+<script script>
+</script>
+
+<template>
+    <div class="hidden md:block">
+        <Ul class="flex space-x-10">
+            <li
+                class="hover:pb-8 hover:border-b-4 hover:text-black cursor-pointer border-tannedOrange text-darkGrayishBlue">
+                Collections</li>
+            <li
+                class="hover:pb-8 hover:border-b-4 hover:text-black cursor-pointer border-tannedOrange text-darkGrayishBlue">
+                Men</li>
+            <li
+                class="hover:pb-8 hover:border-b-4 hover:text-black cursor-pointer border-tannedOrange text-darkGrayishBlue">
+                Women</li>
+        <li
+            class="hover:pb-8 hover:border-b-4 hover:text-black cursor-pointer border-tannedOrange text-darkGrayishBlue">
+            About</li>
+        <li
+            class="hover:pb-8 hover:border-b-4 hover:text-black cursor-pointer border-tannedOrange text-darkGrayishBlue">
+            Contact</li>
+    </Ul>
+</div></template>
---
+++ ecommerce-product-page-main/src/components/NavBar.vue
@@ -0,0 +1,25 @@
+<script setup>
+import DeskNavLinks from '@/components/DeskNavLinks.vue'
+</script>
+
+<template>
+   <div class="container mx-auto px-5 py-3">
+       <div class="flex justify-between ">
+        <div class="flex items-center justify-center space-x-14">
+          <img src="@/images/icon-menu.svg" class="object-none mr-5 cursor-pointer text-blue-500 md:hidden" alt="menu icon">
+          <img src="@/images/logo.svg" class="object-none" alt="Logo">
+          <DeskNavLinks class=""/>
+        </div>
+        
+        <div class="flex space-x-8 justify-end items-center">
+          <div class="relative">
+            <img src="@/images/icon-cart.svg" class="object-none cursor-pointer hover:" alt="cart icon">
+            <p class="bg-tannedOrange px-2 text-white rounded-lg text-[8px] absolute bottom-3 left-2">0</p>
+          </div>
+          <img src="@/images/image-avatar.png" class="w-1/5 border-2 border-white  rounded-full cursor-pointer md:hover:border-tannedOrange md:w-1/4" alt="avatar image">
+        </div>
+         </div>
+         <hr class="hidden mt-8 md:block" />
+   </div>
+  
+</template>
