b28e73a3520f3c9595a0e66acf48c63477bcd167
BerukB <berukberhanua@gmail.com>
2024-01-08 17:34:55 +0300

    phone image design completed

--- ecommerce-product-page-main/src/components/ImageSlider.vue
+++ ecommerce-product-page-main/src/components/ImageSlider.vue
@@ -15,8 +15,8 @@ const props = defineProps({
     <div class="relative ">
         <div class="flex justify-center z-10"><img :src=props.imageUrls[imageIndex] class="w-96 h-72 object-cover" alt=""/></div>
         <div class="absolute w-full h-full flex bottom-0 top-1 justify-between items-center px-4">
-            <img src="../images/icon-previous.svg" class="bg-white px-3 py-2  rounded-full " alt="">
-            <img src="../images/icon-next.svg" class="bg-white px-3 py-2 rounded-full" alt="">
+            <div class="bg-white px-2 py-2 rounded-full" ><img src="../images/icon-previous.svg" class="bg-white px-1 w-5 h-5" alt=""></div>
+            <div class="bg-white px-2 py-2 rounded-full"><img src="../images/icon-next.svg" class="bg-white px-1 w-5 h-5" alt=""></div>
         </div>
     </div>
 </template>
